d019b21b54e38e6e4b8aece5ea364aeec5d2799d
sjs <sjs@sonic.net>
2019-10-23 08:26:02 -0700

    code to be commited to git-hub.
    added license info, README.md, etc.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Notes
+Android Kotlin simple text notepad
+
+At some point this will be a functional simple text notepad.
+I will be adding code from various other projects as I go
+along.  It's written in Kotlin and I'm using it to learn
+Kotlin and the GooglePlay process for android apps.  One of
+the goals is to have an app that will work from API-19
+through whatever is current (API-29 right now)
+
+
+Wed Oct 23 08:20:58 PDT 2019
+Initial commit to git hub.  Current code creates a shortcut
+on the user's home screen if they want one.
--- app/src/main/java/com/kana_tutor/notes/kanautils/CreateShortcut.kt
+++ app/src/main/java/com/kana_tutor/notes/kanautils/CreateShortcut.kt
@@ -18,195 +18,132 @@ package com.kana_tutor.notes.kanautils
  *  and limitations under the License.
  */
 
+/*
+ * This module asks user if they want a shortcut installed on their
+ * home directory.  It is designed to work from API 19 through the
+ * current API-29.
+ */
+
 import android.app.Activity
 import android.app.PendingIntent
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
-import android.os.AsyncTask
 import android.os.Build
-import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 
-
 import android.graphics.drawable.Icon.createWithResource
-import com.kana_tutor.notes.MainActivity
 import com.kana_tutor.notes.R
 
-private lateinit var shortcutId : String
+private lateinit var shortcutId: String
 
+private fun postApi26CreateShortcut(c: Context, scClass: Class<*>) {
+    if (Build.VERSION.SDK_INT >= 26) {
+        val sm = c.getSystemService(ShortcutManager::class.java)
+        if (sm != null && sm.isRequestPinShortcutSupported) {
+            var shortcutExists = false
+            // We create the shortcut multiple times if given the
+            // opportunity.  If the shortcut exists, put up
+            // a toast message and exit.
+            val shortcuts = sm.pinnedShortcuts
+            var i = 0
+            while (i < shortcuts.size && !shortcutExists) {
+                shortcutExists = shortcuts[i].id == shortcutId
+                i++
+            }
+            if (shortcutExists) {
+                Toast.makeText(
+                    c, String.format(
+                        "Shortcut %s already exists.", shortcutId
+                    ), Toast.LENGTH_LONG
+                ).show()
+            } else {
 
+                // this is the intent that actually creates the shortcut.
+                val shortcutIntent = Intent(c, scClass)
+                shortcutIntent.action = shortcutId
+                // this intent is used to wake up the broadcast receiver.
+                // I couldn't get createShortcutResultIntent to work but
+                // just a simple intent as used for a normal broadcast
+                // intent works fine.
+                val broadcastIntent = Intent(shortcutId)
+                broadcastIntent.putExtra("msg", "approve")
 
-class CreateShortcut : Activity() {
-
-    private class WaitFor (N: Int) : AsyncTask<Void, Void, Void>() {
-        val waitPeriod: Int = N * 1000
-        val c = CreateShortcut@this
-        override fun doInBackground(vararg voids: Void): Void? {
-            try {
-                Thread.sleep(waitPeriod.toLong())
-                val bi = Intent(shortcutId)
-                bi.putExtra("msg", "deny")
-                (this as CreateShortcut).sendBroadcast(bi)
-            } catch (ignore: InterruptedException) { /* Ignore */ }
-            return null
-        }
-    }
-
-
-    // internal var kanaUtils: KanaUtils
-    private fun finishActivity(result: String?) {
-        val i = Intent()
-        i.putExtra("result", result)
-        setResult(RESULT_OK, i)
-
-        if (Build.VERSION.SDK_INT >= 21) {
-            finishAndRemoveTask()
-        } else {
-            finish()
-        }
-    }
-
-
-    private fun postApi26CreateShortcut(c: Context, scClass: Class<*>) {
-        if (Build.VERSION.SDK_INT >= 26) {
-            val sm = getSystemService(ShortcutManager::class.java)
-            if (sm != null && sm.isRequestPinShortcutSupported) {
-                var shortcutExists = false
-                // We create the shortcut multiple times if given the
-                // opportunity.  If the shortcut exists, put up
-                // a toast message and exit.
-                val shortcuts = sm.pinnedShortcuts
-                var i = 0
-                while (i < shortcuts.size && !shortcutExists) {
-                    shortcutExists = shortcuts[i].id == shortcutId
-                    i++
-                }
-                if (shortcutExists) {
-                    Toast.makeText(
-                        c, String.format(
-                            "Shortcut %s already exists.", shortcutId
-                        ), Toast.LENGTH_LONG
-                    ).show()
-                    finishActivity("shortcutExists")
-                } else {
-                    // this intent is used to wake up the broadcast receiver.
-                    // I couldn't get createShortcutResultIntent to work but
-                    // just a simple intent as used for a normal broadcast
-                    // intent works fine.
-                    val broadcastIntent = Intent(shortcutId)
-                    broadcastIntent.putExtra("msg", "approve")
-                    // wait up to N seconds for user input, then continue
-                    // on assuming user's choice was deny.
-                    val waitFor = WaitFor(10).execute()
-                    // create an anonymous broadcaster.  Unregister when done.
-                    registerReceiver(
-                        object : BroadcastReceiver() {
-                            override fun onReceive(c: Context, intent: Intent) {
-                                var msg = intent.getStringExtra("msg")
-                                if (msg == null) msg = "NULL"
-                                unregisterReceiver(this)
-                                waitFor.cancel(true)
-                                Log.d(
-                                    "postApi26CreateShortcut", String.format(
-                                        "ShortcutReceiver activity = \"$1%s\" : msg = %s",
-                                        intent.action,
-                                        msg
-                                    )
-                                )
-                                finishActivity(msg)
-                            }
-                        }, IntentFilter(shortcutId)
-                    )
-
-                    // this is the intent that actually creates the shortcut.
-                    val shortcutIntent = Intent(c, scClass)
-                    shortcutIntent.action = shortcutId
-                    val shortcutInfo = ShortcutInfo.Builder(c,
-                        shortcutId
-                    )
-                        .setShortLabel(c.getString(R.string.app_name))
-                        .setIcon(createWithResource(c, R.drawable.qmark))
-                        .setIntent(shortcutIntent)
-                        .build()
-                    val successCallback = PendingIntent.getBroadcast(
-                        c, 99, broadcastIntent, 0
-                    )
-                    // Shortcut gets created here.
-                    sm.requestPinShortcut(shortcutInfo, successCallback.intentSender)
-                }
+                val shortcutInfo = ShortcutInfo.Builder(
+                    c,
+                    shortcutId
+                )
+                    .setShortLabel(c.getString(R.string.app_name))
+                    .setIcon(createWithResource(c, R.drawable.qmark))
+                    .setIntent(shortcutIntent)
+                    .build()
+                val successCallback = PendingIntent.getBroadcast(
+                    c, 99, broadcastIntent, 0
+                )
+                // Shortcut gets created here.
+                sm.requestPinShortcut(shortcutInfo, successCallback.intentSender)
             }
         }
     }
-
-    @Suppress("DEPRECATION")
-    private fun preApi26CreateShortcut(activity: Activity, scClass: Class<*>) {
-        val shortcutIntent = Intent(applicationContext, scClass)
-        shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-
-        val addIntent = Intent()
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent)
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, activity.getString(R.string.app_name))
-        val icon = Intent.ShortcutIconResource.fromContext(
-            activity,
-            R.drawable.qmark
-        )
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon)
-        addIntent.action = "com.android.launcher.action.INSTALL_SHORTCUT"
-        addIntent.putExtra("duplicate", false)
-        activity.sendBroadcast(addIntent)
-        Toast.makeText(activity, "Created pre-api26 shortcut", Toast.LENGTH_LONG)
-            .show()
-        finishActivity("allow")
-    }
-
-    private fun createShortcut(activity: Activity, scClass: Class<*>) {
-        if (Build.VERSION.SDK_INT >= 26)
-            postApi26CreateShortcut(activity, scClass)
-        else
-            preApi26CreateShortcut(activity, scClass)
-    }
-
-    private fun promptForShortcut(activity: Activity, scClass: Class<*>) {
-        Log.d("promptForShortcut", "Called")
-        val buttonIds = intArrayOf(
-            R.string.NO,
-            R.string.YES
-        )
-        val buttonCallbacks: Array<() -> Unit>  =
-            arrayOf(
-                { // Toast.makeText(activity, "No", Toast.LENGTH_LONG).show()
-                    finishActivity("deny")
-                },
-                {  // Toast.makeText(activity, "Yes", Toast.LENGTH_LONG).show()
-                    createShortcut(activity, scClass)
-                }
-            )
-        val promptMess = getString(
-            R.string.promptForShortcut, getString(R.string.app_name)
-        )
-        yesNoDialog(
-            activity,
-            false,
-            promptMess,
-            buttonIds,
-            buttonCallbacks
-        )
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.create_shortcut)
-        val i = intent
-        val scid = i.getStringExtra("shortcutId")
-        shortcutId = scid ?: "NULL"
-        intent = null
-        // kanaUtils = KanaUtils(this)
-        promptForShortcut(this, MainActivity::class.java)
-    }
 }
+
+@Suppress("DEPRECATION")
+private fun preApi26CreateShortcut(activity: Activity, scClass: Class<*>) {
+    val shortcutIntent = Intent(activity, scClass)
+    shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+    val addIntent = Intent()
+    addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent)
+    addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, activity.getString(R.string.app_name))
+    val icon = Intent.ShortcutIconResource.fromContext(
+        activity,
+        R.drawable.qmark
+    )
+    addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon)
+    addIntent.action = "com.android.launcher.action.INSTALL_SHORTCUT"
+    addIntent.putExtra("duplicate", false)
+    activity.sendBroadcast(addIntent)
+    Toast.makeText(activity, "Created pre-api26 shortcut", Toast.LENGTH_LONG)
+        .show()
+}
+
+private fun createShortcut(activity: Activity, scClass: Class<*>) {
+    if (Build.VERSION.SDK_INT >= 26)
+        postApi26CreateShortcut(activity, scClass)
+    else
+        preApi26CreateShortcut(activity, scClass)
+}
+
+fun promptForShortcut(activity: Activity, scClass: Class<*>) {
+    Log.d("promptForShortcut", "Called")
+    val buttonIds = intArrayOf(
+        R.string.NO,
+        R.string.YES
+    )
+    val buttonCallbacks: Array<() -> Unit> =
+        arrayOf(
+            {
+                // Toast.makeText(activity, "No", Toast.LENGTH_LONG).show()
+                Toast.makeText(activity, "declined", Toast.LENGTH_LONG).show()
+            },
+            {
+                // Toast.makeText(activity, "Yes", Toast.LENGTH_LONG).show()
+                createShortcut(activity, scClass)
+            }
+        )
+    val promptMess = activity.getString(
+        R.string.promptForShortcut, activity.getString(R.string.app_name)
+    )
+    shortcutId = activity.getString(R.string.app_name)
+    yesNoDialog(
+        activity,
+        false,
+        promptMess,
+        buttonIds,
+        buttonCallbacks
+    )
+}
+
