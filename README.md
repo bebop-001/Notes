428508cbce202f182feca6f3eff07fe863d2aae4
sjs <sjs@sonic.net>
2019-11-13 12:43:22 -0800

    first working version.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Notes
-Android Kotlin simple text notepad
+Android Kotlin simple text notepad using Storage Access Framework
 
 At some point this will be a functional simple text notepad.
 I will be adding code from various other projects as I go
@@ -12,3 +12,17 @@ through whatever is current (API-29 right now)
 Wed Oct 23 08:20:58 PDT 2019
 Initial commit to git hub.  Current code creates a shortcut
 on the user's home screen if they want one.
+
+Wed Nov 13 12:31:27 PST 2019
+Lots of changes.   Basic functionality is there I think.
+
+App is based on StorageDemo source code discussed in 
+the Kotlin edition of [Android Studio 3.5 Essentials]
+(https://www.ebookfrenzy.com/ebookpages/kotlin_android_studio_35_ebook.html).
+I've used several of this series and they've all been a good value.
+
+At this point, the UI is funky but it's working well enough I've 
+started to use it.
+
+It uses Google's Storage Access Framework (SAF) and should work
+through Android 10.
--- app/src/main/java/com/kana_tutor/notes/MainActivity.kt
+++ app/src/main/java/com/kana_tutor/notes/MainActivity.kt
@@ -15,50 +15,188 @@
  *  See the License for the specific language governing permissions
  *  and limitations under the License.
  */
+@file:Suppress("SpellCheckingInspection")
 
 package com.kana_tutor.notes
 
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.view.View
+import android.net.Uri
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import com.kana_tutor.notes.kanautils.promptForShortcut
+
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val CREATE_SHORTCUT = 99
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CREATE_SHORTCUT) {
-            if (resultCode == Activity.RESULT_OK) {
-                val result = data!!.getStringExtra("result")
-                Log.d("MainActivity", "CreateShortcut result:" + result!!)
-            }
-            else if (resultCode == Activity.RESULT_CANCELED) {
-                //Write your code if there's no result
-            }
-        }
-
-    }
 
     private lateinit var _userPreferences : SharedPreferences
     val userPreferences : SharedPreferences
     get() = _userPreferences
 
+    private val CREATE_REQUEST_CODE = 40
+    private val OPEN_REQUEST_CODE = 41
+    private val SAVE_REQUEST_CODE = 42
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         _userPreferences = getSharedPreferences(
-            getString(R.string.app_name) + ".user_preferences", Context.MODE_PRIVATE
+            getString(R.string.app_name) + ".user_preferences"
+            , Context.MODE_PRIVATE
         )
         var firstRun = userPreferences.getBoolean("firstRun", true)
         if (firstRun || true) {
             userPreferences.edit()
                 .putBoolean("firstRun", false)
                 .apply()
-            com.kana_tutor.notes.kanautils.promptForShortcut(this, MainActivity::class.java)
+            // Ask user if they want a shortcut on their home screen.
+            promptForShortcut(this, MainActivity::class.java)
+        }
+
+        fileText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int, after: Int) {
+                Log.d("TextListener", "beforeTextChanged")
+            }
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int, count: Int) {
+                Log.d("TextListener", "onTextChanged")
+            }
+            override fun afterTextChanged(s: Editable) {
+                Log.d("TextListener", "afterTextChanged")
+            }
+        })
+        // Things work ok with listener commented out.  for demo only.
+        fileText.setOnTouchListener { v, event ->
+            Log.d("OnTouch:", String.format("event=%s", event.toString()))
+            // imm.showSoftInput(v, InputMethodManager.SHOW_FORCED)
+            false // set false indicating listener handeled event.
+        }
+        // for demo
+        fileText.setOnFocusChangeListener { v, hasFocus ->
+            Log.d("OnFocusChangeListener"
+                , String.format("hasFocus:%s", hasFocus.toString()))
+        }
+    }
+
+    // view declaration necessary for API 19 default button callback
+    fun newFile(view: View) {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TITLE, "newfile.txt")
+
+        startActivityForResult(intent, CREATE_REQUEST_CODE)
+    }
+    // view declaration necessary for API 19 default button callback
+    fun saveFile(view: View) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "text/plain"
+
+        startActivityForResult(intent, SAVE_REQUEST_CODE)
+    }
+
+    private fun writeFileContent(uri: Uri) {
+        try {
+            val pfd = contentResolver.openFileDescriptor(uri, "w")
+
+            val fileOutputStream = FileOutputStream(
+                pfd?.fileDescriptor)
+
+            val textContent = fileText.text.toString()
+
+            fileOutputStream.write(textContent.toByteArray())
+
+            fileOutputStream.close()
+            pfd?.close()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    // view declaration necessary for API 19 default button callback
+    fun openFile(view: View) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "text/plain"
+        startActivityForResult(intent, OPEN_REQUEST_CODE)
+    }
+
+    private fun readFileContent(uri: Uri): String {
+
+        val inputStream = contentResolver.openInputStream(uri)
+        val reader = BufferedReader(InputStreamReader(
+            inputStream))
+        val stringBuilder = StringBuilder()
+
+
+        var currentline = reader.readLine()
+
+        while (currentline != null) {
+            stringBuilder.append(currentline + "\n")
+            currentline = reader.readLine()
+        }
+        inputStream?.close()
+        return stringBuilder.toString()
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int,
+                                         resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+
+        var currentUri: Uri?
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CREATE_REQUEST_CODE -> {
+                    if (resultData != null) {
+                        fileText.setText("")
+                    }
+                }
+                SAVE_REQUEST_CODE -> {
+                    resultData?.let {
+                        currentUri = it.data
+                        currentUri?.let {
+                            writeFileContent(it)
+                        }
+                    }
+                }
+                OPEN_REQUEST_CODE -> {
+                    resultData?.let {
+                        currentUri = it.data
+                        currentUri?.let {
+                            try {
+                                val content = readFileContent(it)
+                                fileText.setText(content)
+                                // fileText.requestFocus()
+                            } catch (e: IOException) {
+                                throw RuntimeException("Open failed:" + e.message + e.stackTrace)
+                            }
+                        }
+                    }
+                }
+                else -> throw RuntimeException(String.format("onActivityResult" +
+                            "Unexpected request code: 0x%08x", requestCode
+                    )
+                )
+            }
         }
     }
 }
